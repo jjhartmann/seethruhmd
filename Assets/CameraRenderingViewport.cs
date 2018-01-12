0eb13538b13c809c7b435e2f8b040881d00f45b1
jhartman <jhartman@sfu.ca>
2018-01-12 02:00:33 -0500

    Add camera rendering texture

---
+++ Assets/CameraRenderingViewport.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraRenderingViewport : MonoBehaviour
+{
+
+
+    public WebCamDevice[] devices;
+    public WebCamTexture mCamera;
+    public GameObject plane;
+    public RawImage background;
+
+    // Use this for initialization
+    void Start () {
+
+        devices = WebCamTexture.devices;
+        if (devices.Length == 0)
+        {
+            Debug.Log("No Cameras");
+            return;
+           
+        }
+
+        for (int i = 0; i < devices.Length; ++i)
+        {
+            if (!devices[i].isFrontFacing)
+            {
+                mCamera = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+        if (mCamera == null) return;
+        
+        mCamera.Play();
+        if (plane != null)
+        {
+            plane.GetComponent<Renderer>().material.mainTexture = mCamera;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
